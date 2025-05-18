bd88be1909ce1162c1cf32e1c4c69300f722b1b6
tanni6808 <tanni6808450@gmail.com>
2025-05-18 23:44:13 +0800

    First demo version

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { useState, useEffect, useRef } from "react";
 import { Words } from "./lib/definitions";
 
 export default function Home() {
-  const [maxWords, setMaxWords] = useState<number>(100);
-  const [maxFontSize, setMaxFontSize] = useState<number>(96);
-  const [minFontSize, setMinFontSize] = useState<number>(12);
+  const maxFontSize = 96;
+  const minFontSize = 12;
+  const [addWord, setAddWord] = useState<string>("");
+  const [customizedWords, setCustomizedWords] = useState<string[]>([]);
+  const [maxWords, setMaxWords] = useState<number>(50);
   const [stopwords, setStopwords] = useState<Set<string>>(new Set());
   const [article, setArticle] = useState<string>("");
   const [result, setResult] = useState<Words[]>([]);
@@ -13,52 +15,62 @@ export default function Home() {
   const isWordCloudAppend = useRef(false);
   const svgRef = useRef<SVGSVGElement>(null);
 
+  const handleDlSvg = () => {
+    if (!svgRef.current) return;
+
+    const svgElement = svgRef.current;
+    const svgData = new XMLSerializer().serializeToString(svgElement);
+    const blob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "makeWordCloud.svg";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
+
+  const handleReset = () => {
+    if (confirm("目前的進度將會全部被清除，是否繼續？")) {
+      if (!svgRef.current) return;
+      const svg = svgRef.current;
+      svg.innerHTML = "";
+      isWordCloudAppend.current = false;
+      setAddWord("");
+      setCustomizedWords([]);
+      setResult([]);
+    }
+  };
+
+  const handleDelCustomizedWord = (word: string) => {
+    setCustomizedWords((prev) => prev.filter((w) => w !== word));
+  };
+
   const handleSubmit = () => {
+    if (!article.trim()) return alert("請輸入文章!");
     isWordCloudAppend.current = true;
-    const segmenter = new Intl.Segmenter("zh-tw", { granularity: "word" });
-    // 進行斷詞
-    const rawWords = Array.from(segmenter.segment(article))
-      .filter((segment) => segment.isWordLike)
-      .map((segment) => segment.segment.trim());
+    const drawWords = generateWordArr(customizedWords, article, stopwords);
 
-    // 計算每個詞出現的次數
-    const words: Words[] = [];
-    rawWords.forEach((rawWord) => {
-      if (stopwords.has(rawWord)) return; // 去除停用詞
-      let inArr = false;
-      words.forEach((word) => {
-        if (rawWord === word.word) {
-          inArr = true;
-          word.count++;
-        }
-      });
-      if (!inArr) words.push({ word: rawWord, count: 1, size: 1 });
-    });
-    // 將結果從出現次數多到少排列
-    words.sort((a, b) => b.count - a.count);
-
-    // // 取得前100個詞；頻率較低的詞隨機抽取
-    let drawWords: Words[] = [];
-    const countThreshold = words[99].count; // 第100個詞的出現次數
-    if (words[100].count === countThreshold) {
-      const thresholdIndex = words.findIndex(
-        (word) => word.count === countThreshold
-      );
-      const wordsAboveTh = words.slice(0, thresholdIndex);
-      const wordsAtTh = words.filter((word) => word.count === countThreshold);
-      wordsAtTh.sort(() => Math.random() - 0.5);
-      drawWords = [...wordsAboveTh, ...wordsAtTh].slice(0, 100);
-    } else drawWords = words.slice(0, 100);
-
-    // 輸出結果
     setResult(drawWords);
+  };
+
+  const handleAddCustomizedWord = () => {
+    const clearWord = addWord.trim();
+    if (!clearWord) return alert("請輸入詞彙。");
+    if (result.filter((w) => w.word === clearWord).length !== 0)
+      return alert(
+        `「${clearWord}」已經被使用；若找不到該詞彙，有可能是因為出現次數過少而未被顯示。`
+      );
+    setCustomizedWords((prev) => [...prev, addWord]);
   };
 
   // 取得範本/停用詞
   useEffect(() => {
     if (!isEffectCalledRef.current) {
       isEffectCalledRef.current = true;
-      fetch("/texts/pigeon.txt") //<================== change text
+      fetch("/texts/word-cloud.txt") //<================== change text
         .then((res) => res.text())
         .then((text) => setArticle(text));
       fetch("/stopwords.txt")
@@ -72,6 +84,14 @@ export default function Home() {
         });
     }
   }, []);
+
+  // 更新自訂詞彙
+  useEffect(() => {
+    if (!isWordCloudAppend.current) return;
+    const drawWords = generateWordArr(customizedWords, article, stopwords);
+    setResult(drawWords);
+    setAddWord("");
+  }, [customizedWords]);
 
   // 進行文字雲繪製
   useEffect(() => {
@@ -176,34 +196,11 @@ export default function Home() {
       "transform",
       `translate(${translateX}, ${translateY}) scale(${scale})`
     );
-  }, [[result], maxWords, maxFontSize, minFontSize]);
+  }, [result, maxWords]);
 
   return (
     <div className="flex">
       <div className="w-1/3">
-        <div className="flex">
-          <div className="w-full h-80 border-2 border-blue-800 m-2 overflow-y-scroll">
-            {result.slice(0, maxWords).map((word, index) => (
-              <div
-                key={index}
-                className="flex p-2 justify-between hover:bg-blue-100"
-              >
-                <div>{`${word.word}, Counts: ${word.count}`}</div>
-              </div>
-            ))}
-          </div>
-          {/* <div className="w-2/4 h-80 border-2 border-red-800 m-2 overflow-y-scroll">
-            {result.slice(maxWords).map((word, index) => (
-              <div
-                key={index}
-                className="flex p-2 justify-between hover:bg-blue-100"
-              >
-                <div>{`${word.word}, Counts: ${word.count}`}</div>
-              </div>
-            ))}
-          </div> */}
-        </div>
-
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -211,70 +208,147 @@ export default function Home() {
           }}
           className="m-2 flex flex-col"
         >
+          <div className={isWordCloudAppend.current ? "hidden" : ""}>
+            輸入文章：
+          </div>
           <textarea
             name=""
             id=""
             value={article}
             onChange={(e) => setArticle(e.target.value)}
-            className="h-40 border-2 border-gray-500"
+            className={`${
+              isWordCloudAppend.current ? "hidden" : ""
+            } h-40 border-2 border-gray-500`}
+            required
           ></textarea>
-          <button
-            type="submit"
-            className="w-20 h-10 border-1 my-2 hover:bg-gray-300"
-          >
-            Go
-          </button>
-        </form>
-      </div>
-      <div className="">
-        <svg ref={svgRef}></svg>
-        <div className={isWordCloudAppend.current ? "" : "hidden"}>
-          <div className="m-2">
-            <label htmlFor="">關鍵詞未被正確切割？手動新增：</label>
-            <input type="text" className="border-1" />
-            <button className="border-1 hover:bg-gray-300">新增</button>
+          <div className="flex">
+            <button
+              type="submit"
+              className="w-20 h-10 border-1 my-2 hover:bg-gray-300"
+            >
+              {isWordCloudAppend.current ? "重新排列" : "開始繪製"}
+            </button>
+            <button
+              type="button"
+              onClick={handleReset}
+              className={`${
+                isWordCloudAppend.current ? "" : "hidden"
+              } w-20 h-10 border-1 my-2 ml-4 text-red-600 hover:bg-red-300`}
+            >
+              修改文章
+            </button>
           </div>
+        </form>
+        <div className={`${isWordCloudAppend.current ? "" : "hidden"} flex`}>
+          <div className="w-2/4 m-2">
+            <div className="">詞彙庫：</div>
+            <div className=" h-80 border-2 border-blue-800 overflow-y-scroll">
+              {result
+                .slice(0, maxWords)
+                .filter((w) => !new Set(customizedWords).has(w.word))
+                .map((word, index) => (
+                  <div
+                    key={index}
+                    className="flex p-2 justify-between hover:bg-blue-100"
+                  >
+                    <div>{`${word.word}, Counts: ${word.count}`}</div>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="w-2/4 m-2">
+            <div className="">自訂詞彙：</div>
+            <div className="h-80 border-2 border-red-800 overflow-y-scroll">
+              {result
+                .slice(0, maxWords)
+                .filter((w) => new Set(customizedWords).has(w.word))
+                .map((word, index) => (
+                  <div
+                    key={index}
+                    className="flex p-2 justify-between hover:bg-blue-100"
+                  >
+                    <div>{`${word.word}, Counts: ${word.count}`}</div>
+                    <button
+                      onClick={() => handleDelCustomizedWord(word.word)}
+                      className="border-1 hover:bg-gray-300 px-1"
+                    >
+                      刪除
+                    </button>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className={isWordCloudAppend.current ? "" : "hidden"}>
+          <form
+            className="m-2"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleAddCustomizedWord();
+            }}
+          >
+            <div className="">關鍵詞未被正確切割？</div>
+            <label htmlFor="">手動新增：</label>
+            <input
+              type="text"
+              className="border-1"
+              value={addWord}
+              onChange={(e) => setAddWord(e.target.value)}
+            />
+            <button type="submit" className="border-1 hover:bg-gray-300 px-1">
+              新增
+            </button>
+          </form>
           <div className="flex">
             <div className="m-2">
-              <label htmlFor="">字詞數量：</label>
-              <input
-                type="number"
-                name=""
-                id=""
-                className="border-1 w-10"
-                min={10}
-                max={100}
-                value={maxWords}
-                onChange={(e) => setMaxWords(Number(e.target.value))}
-              />
+              <label htmlFor="">字詞數量（10-100）：</label>
+              <button
+                onClick={() => setMaxWords((prevMaxWords) => prevMaxWords - 10)}
+                className="border-1 mx-1 px-1 hover:bg-gray-300 disabled:text-gray-500 disabled:hover:bg-white"
+                disabled={maxWords < 20 ? true : false}
+              >
+                -10
+              </button>
+              <button
+                onClick={() => setMaxWords((prevMaxWords) => prevMaxWords - 1)}
+                className="border-1 mx-1 px-1 hover:bg-gray-300 disabled:text-gray-500 disabled:hover:bg-white"
+                disabled={maxWords < 11 ? true : false}
+              >
+                -1
+              </button>
+              <span className="font-bold">{maxWords}</span>
+              <button
+                onClick={() => setMaxWords((prevMaxWords) => prevMaxWords + 1)}
+                className="border-1 mx-1 px-1 hover:bg-gray-300 disabled:text-gray-500 disabled:hover:bg-white"
+                disabled={
+                  maxWords > 99 || maxWords > result.length - 1 ? true : false
+                }
+              >
+                +1
+              </button>
+              <button
+                onClick={() => setMaxWords((prevMaxWords) => prevMaxWords + 10)}
+                className="border-1 mx-1 px-1 hover:bg-gray-300 disabled:text-gray-500 disabled:hover:bg-white"
+                disabled={
+                  maxWords > 90 || maxWords > result.length - 10 ? true : false
+                }
+              >
+                +10
+              </button>
             </div>
-            {/* <div className="m-2">
-              <label htmlFor="">最大字體：</label>
-              <input
-                type="number"
-                name=""
-                id=""
-                className="border-1 w-12"
-                min={minFontSize}
-                max={300}
-                value={maxFontSize}
-                onChange={(e) => setMaxFontSize(Number(e.target.value))}
-              />
-            </div>
-            <div className="m-2">
-              <label htmlFor="">最小字體：</label>
-              <input
-                type="number"
-                name=""
-                id=""
-                className="border-1 w-12"
-                min={10}
-                max={maxFontSize}
-                value={minFontSize}
-                onChange={(e) => setMinFontSize(Number(e.target.value))}
-              />
-            </div> */}
           </div>
+        </div>
+      </div>
+      <div className="m-2">
+        <svg ref={svgRef}></svg>
+        <div className={isWordCloudAppend.current ? "flex" : "hidden"}>
+          <div className="">下載：</div>
+          <button
+            onClick={handleDlSvg}
+            className="border-1 hover:bg-gray-300 px-1"
+          >
+            SVG
+          </button>
         </div>
       </div>
     </div>
@@ -288,4 +362,69 @@ function isOverlap(a: DOMRect, b: DOMRect) {
     a.y + a.height < b.y ||
     a.y > b.y + b.height
   );
+}
+
+function generateWordArr(
+  prevArr: string[],
+  article: string,
+  stopwords: Set<string>
+) {
+  let keywordCounts: Words[] = [];
+  let processedArticle = article;
+  if (prevArr.length !== 0) {
+    prevArr.forEach((keyword) => {
+      const regex = new RegExp(keyword, "gi");
+      const matches = processedArticle.match(regex);
+
+      if (matches) {
+        keywordCounts.push({ word: keyword, count: matches.length, size: 1 });
+        processedArticle = processedArticle.replaceAll(regex, " ");
+      } else
+        return alert(
+          `找不到「${keyword}」。如果您確定文章中有這個詞，請檢查是否曾新增了過於簡短或重疊的自訂詞，導致詞彙被切割。建議刪除可能衝突的自訂詞，或直接清除所有自訂詞後再試一次。`
+        );
+    });
+  }
+
+  const segmenter = new Intl.Segmenter("zh-tw", { granularity: "word" });
+  // 進行斷詞
+  const rawWords = Array.from(segmenter.segment(processedArticle))
+    .filter((segment) => segment.isWordLike)
+    .map((segment) => segment.segment.trim());
+
+  // 計算每個詞出現的次數
+  const words: Words[] = [];
+  rawWords.forEach((rawWord) => {
+    if (stopwords.has(rawWord)) return; // 去除停用詞
+    let inArr = false;
+    words.forEach((word) => {
+      if (rawWord === word.word) {
+        inArr = true;
+        word.count++;
+      }
+    });
+    if (!inArr) words.push({ word: rawWord, count: 1, size: 1 });
+  });
+  const allWords = [...keywordCounts, ...words];
+  // 將結果從出現次數多到少排列
+  allWords.sort((a, b) => b.count - a.count);
+
+  // // 取得前100個詞；頻率較低的詞隨機抽取
+  let drawWords: Words[] = [];
+  if (allWords.length > 99) {
+    const countThreshold = allWords[99].count; // 第100個詞的出現次數
+    if (allWords[100].count === countThreshold) {
+      const thresholdIndex = allWords.findIndex(
+        (word) => word.count === countThreshold
+      );
+      const wordsAboveTh = allWords.slice(0, thresholdIndex);
+      const wordsAtTh = allWords.filter(
+        (word) => word.count === countThreshold
+      );
+      wordsAtTh.sort(() => Math.random() - 0.5);
+      drawWords = [...wordsAboveTh, ...wordsAtTh].slice(0, 100);
+    }
+  } else drawWords = allWords.slice(0, 100);
+
+  return drawWords;
 }
